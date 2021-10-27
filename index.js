870425b53ac1f4d8bad8d13f69bb92684ea58de0
Alter80 <fahimatul8@Gmail.com>
2021-10-27 19:28:38 +0600

    hello updated2

--- index.js
+++ index.js
@@ -110,12 +110,14 @@ every project do these:
 1. git init
 2. .gitignore(node_module, .env)
 3. push everything to git
-4. make sure you have this written in package.json: "start": "node index.js",
+4. make sure you have this written in package.json:
+    "start": "node index.js",
     "start-dev": "nodemon index.js",
 5. Make sure: put process.env.PORT in front of your port number
 6. heroku login
 7. heroku create (only one time for a project)
 8. command: git push heroku main
+9. heroku> app> settings> add var(add DB_USER, DB_PASS)
 ----
 
 update:
